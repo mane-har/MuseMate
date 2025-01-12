83db16250e83ab914a0ec479490b84d5bd221648
ADMIN <mane.harutyunian99@gmail.com>
2025-01-12 05:33:06 +0400

    first

--- app/src/main/java/com/musemate/MainActivity.java
+++ app/src/main/java/com/musemate/MainActivity.java
@@ -17,39 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, new HomeFrag())
-                    .commit();
-        }
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        // Set default fragment
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFrag()).commit();
 
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            Fragment selectedFragment = null;
-
-            switch (item.getItemId()) {
-                case R.id.nav_home:
-                    selectedFragment = new HomeFrag();
-                    break;
-                case R.id.nav_music:
-                    selectedFragment = new MusicFrag();
-                    break;
-                case R.id.nav_chat:
-                    selectedFragment = new ChatFrag();
-                    break;
-                case R.id.nav_account:
-                    selectedFragment = new AccountFrag();
-                    break;
-            }
-
-            if (selectedFragment != null) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-            }
-
-            return true;
-        });
     }
 }
